00eab97251adfc51436d14f9410968d72bf043ec
yana <ytourokuyouh@gmail.com>
2025-12-03 16:04:52 +0900

    一覧画面, メニュー追加機能, メニュー削除機能の実装

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import random
 
@@ -19,9 +19,34 @@ class Menu(db.Model):
     category = db.Column(db.String(50), nullable=False)
 
 
+# 一覧画面
 @app.route("/")
 def index():
-    return "<h1>Welcome Menu Mate</h1>"
+    menus = Menu.query.all()
+    return render_template("index.html", menus=menus)
+
+
+# メニュー追加機能
+@app.route("/add", methods=["POST"])
+def add_menu():
+    name = request.form.get("name")
+    category = request.form.get("category")
+    # 受け取ったデータをインスタンス化
+    new_menu = Menu(name=name, category=category)
+    db.session.add(new_menu)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+# メニュー削除機能
+@app.route("/delete/<int:id>", methods=["POST"])
+def delete_menu(id):
+    # 指定されたIDのメニューを取得
+    # 存在しない場合は404エラーを返す
+    menu = Menu.query.get_or_404(id)
+    db.session.delete(menu)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 
 # アプリの起動
